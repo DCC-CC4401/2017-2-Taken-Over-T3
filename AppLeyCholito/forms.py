023a95c39d5945a0950d954c46a12238a6982b65
mcaballeroguillen <mcaballeroguillen@gmail.com>
2017-10-03 01:14:30 -0300

    Se agrego los checkbox

--- AppLeyCholito/forms.py
+++ AppLeyCholito/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Denuncia
 
+TIPOS_SEXO=(('H',"Hembra"),('M',"Macho"))
+
 class DenunciaForm(forms.ModelForm):
+    Herido = forms.BooleanField()
+    Sexo = forms.MultipleChoiceField(required=True,
+        widget=forms.CheckboxSelectMultiple,
+        choices=TIPOS_SEXO,
+    )
     class Meta:
         model= Denuncia
-        fields = ('TipDenucia', 'Animal','Sexo','Color','Comentario')
+        fields = ('TipDenucia', 'Animal','Sexo','Herido','Color','Comentario')
