5c98b99b6eb65ff1e371c6a1219fa33a86b10917
gpilleux <guillermo.pilleux@ing.uchile.cl>
2017-10-18 23:02:01 -0300

    register+login+bootstrap

--- AppLeyCholito/forms.py
+++ AppLeyCholito/forms.py
@@ -14,7 +14,11 @@ user_type_options = (('PN', 'Persona Natural'),
                  ('RM', 'Representante de Municipalidad'))
 
 class DenunciaForm(forms.ModelForm):
-    TipDenuncia = forms.ChoiceField(choices=TIPOS_OPCIONES, widget=forms.Select(attrs={"class": "form-control"}))
+    TipDenuncia = forms.ChoiceField(choices=TIPOS_OPCIONES, widget=forms.Select(
+        attrs={
+            "class": "form-control",
+        })
+    )
     Animal = forms.ChoiceField(choices=TIPOS_ANIMALES, widget=forms.Select(attrs={"class": "form-control"}))
     Sexo = forms.ChoiceField(TIPOS_SEXO, widget=forms.Select(attrs={"class": "form-control"}))
     Color = forms.CharField(max_length=10, widget=forms.TextInput(
@@ -39,18 +43,46 @@ class DenunciaForm(forms.ModelForm):
 
 
 class AuthenticationForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Contraseña'
+        }
+    ))
+    password_repeat = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Repite la Contraseña'
+        }
+    ))
+    user_type = forms.ChoiceField(choices=user_type_options, widget=forms.Select(
+        attrs={
+            "class": "form-control",
+        })
+                                    )
     class Meta:
         model = Authentication
-        widgets = {
-            'password': forms.PasswordInput(),
-            'password_repeat': forms.PasswordInput(),
-        }
         fields = ('username', 'password', 'password_repeat', 'user_type')
 
 class LoginForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Contraseña'
+        }
+    ))
     class Meta:
         model = Login
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
         fields = ('username', 'password')
